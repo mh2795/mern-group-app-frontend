26ff0c6f3460d8e22a28e2a16fa7e93bbff643fe
Mustafa Hassan <mustafawhassan@gmail.com>
2019-11-19 15:08:08 -0500

    Added create forms for individual entries

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import Navbar from "./components/navbar.component";
 import Dashboard from "./components/dashboard.component";
 import SkillsList from "./components/skills-list.component";
 // import EditSkill from "./components/edit-skill.component";
-// import CreateSkill from "./components/create-skill.component";
+import CreateSkill from "./components/create-skill.component";
 import AgendasList from "./components/agendas-list.component";
 // import EditAgenda from "./components/edit-agenda.component";
-// import CreateAgenda from "./components/create-agenda.component";
+import CreateAgenda from "./components/create-agenda.component";
 import ApplicationsList from "./components/applications-list.component";
 // import EditApplication from "./components/edit-application.component";
-// import CreateApplication from "./components/create-application.component";
+import CreateApplication from "./components/create-application.component";
 
 function App() {
   return (
@@ -21,15 +21,15 @@ function App() {
       <Navbar />
       <br/>
       <Route path="/" exact component={Dashboard} />
-      <Route path="/skill" component={SkillsList} />
-      {/* <Route path="/skill/edit/:id" component={EditSkill} />
-      <Route path="/skill/create" component={CreateSkill} /> */}
+      <Route path="/skill" exact component={SkillsList} />
+      {/* <Route path="/skill/edit/:id" component={EditSkill} /> */}
+      <Route path="/skill/create" component={CreateSkill} />
       <Route path="/agenda" exact component={AgendasList} />
-      {/* <Route path="/agenda/edit/:id" component={EditAgenda} />
-      <Route path="/agenda/create" component={CreateAgenda} /> */}
+      {/* <Route path="/agenda/edit/:id" component={EditAgenda} /> */}
+      <Route path="/agenda/create" component={CreateAgenda} />
       <Route path="/application" exact component={ApplicationsList} />
-      {/* <Route path="/application/edit/:id" component={EditApplication} />
-      <Route path="/application/create" component={CreateApplication} /> */}
+      {/* <Route path="/application/edit/:id" component={EditApplication} /> */}
+      <Route path="/application/create" component={CreateApplication} />
 
       </div>
     </Router>
---
+++ src/components/create-application.component.js
@@ -0,0 +1,131 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class CreateApplication extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onChangeCompany = this.onChangeCompany.bind(this);
+    this.onChangePosition = this.onChangePosition.bind(this);
+    this.onChangeLink = this.onChangeLink.bind(this);
+    this.onChangeRate = this.onChangeRate.bind(this);
+    this.onChangeStatus = this.onChangeStatus.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      company: '',
+      position: '',
+      link: '',
+      rate: '',
+      status: '',
+    }
+  }
+
+  onChangeCompany(e) {
+    this.setState({
+      company: e.target.value
+    })
+  }
+
+  onChangePosition(e) {
+    this.setState({
+      position: e.target.value
+    })
+  }
+
+  onChangeLink(e) {
+    this.setState({
+      link: e.target.value
+    })
+  }
+
+  onChangeRate(e) {
+    this.setState({
+      rate: e.target.value
+    })
+  }
+
+  onChangeStatus(e) {
+    this.setState({
+      status: e.target.value
+    })
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const application = {
+      company: this.state.company,
+      position: this.state.position,
+      link: this.state.link,
+      rate: this.state.rate,
+      status: this.state.status,
+    }
+
+    console.log(application);
+
+    axios.post('https://personal-tracker-mrt.herokuapp.com/application', application)
+      .then(res => console.log(res.data));
+
+    window.location = '/';
+  }
+
+  render() {
+    return (
+    <div>
+      <h3>Add New Application</h3>
+      <form onSubmit={this.onSubmit}>
+      <div className="form-group"> 
+          <label>Company: </label>
+          <input  type="text"
+              required
+              className="form-control"
+              value={this.state.company}
+              onChange={this.onChangeCompany}
+              />
+        </div>
+        <div className="form-group"> 
+          <label>Position: </label>
+          <input  type="text"
+              required
+              className="form-control"
+              value={this.state.position}
+              onChange={this.onChangePosition}
+              />
+        </div>
+        <div className="form-group">
+          <label>Link: </label>
+          <input 
+              type="text" 
+              className="form-control"
+              value={this.state.link}
+              onChange={this.onChangeLink}
+              />
+        </div>
+        <div className="form-group">
+          <label>Rate: </label>
+          <input 
+              type="text" 
+              className="form-control"
+              value={this.state.rate}
+              onChange={this.onChangeRate}
+              />
+        </div>
+        <div className="form-group">
+          <label>Status: </label>
+          <input 
+              type="text" 
+              className="form-control"
+              value={this.state.status}
+              onChange={this.onChangeStatus}
+              />
+        </div>
+
+        <div className="form-group">
+          <input type="submit" value="Save Application" className="btn btn-primary" />
+        </div>
+      </form>
+    </div>
+    )
+  }
+}
