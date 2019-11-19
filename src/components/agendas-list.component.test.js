6eff095fc1c69da45f4420851dcdf042d7fcce59
Tissa Khosla <tissa.music@gmail.com>
2019-11-19 14:18:51 -0500

    adjustment of test files

--- src/components/agendas-list.component.test.js
+++ src/components/agendas-list.component.test.js
@@ -3,13 +3,9 @@ import { shallow } from 'enzyme'
 
 import Agenda from './agendas-list.component'
 
-// We will describe a block of tests
 describe('Agenda list component', () => {
-	// we will write one individual test
   it('should render as expected', () => {
-    // Shallow rendering renders a component without rendering any of its children
     const component = shallow(<Agenda/>)
-    // We create an assertion within the test that checks if our component renders our name prop
     expect(component.contains(<h3>Agenda</h3>)).toBe(true)
   })
 })
--- src/components/applications-list.components.test.js
+++ src/components/applications-list.components.test.js
@@ -3,13 +3,9 @@ import { shallow } from 'enzyme'
 
 import Applications from './applications-list.component'
 
-// We will describe a block of tests
 describe('Application list component', () => {
-	// we will write one individual test
   it('should render as expected', () => {
-    // Shallow rendering renders a component without rendering any of its children
     const component = shallow(<Applications/>)
-    // We create an assertion within the test that checks if our component renders our name prop
     expect(component.contains(<h3>Applications</h3>)).toBe(true)
   })
 })
