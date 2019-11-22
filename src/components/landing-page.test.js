39935503e2b1e93e2331386c9753f0f5ff48eadf
Tissa Khosla <tissa.music@gmail.com>
2019-11-22 13:50:55 -0500

    OTOI

--- src/components/landing-page.test.js
+++ src/components/landing-page.test.js
@@ -6,6 +6,6 @@ import LandingPage from './landing-page.component.js'
 describe('LandingPage component', () => {
   it('should render a header with the h1 element', () => {
     const component = shallow(<LandingPage/>)
-    expect(component.contains(<h3>Personal Tracker</h3>).toBe(true))
+    expect(component.contains(<h1>OTOI</h1>).toBe(true))
   })
 })
